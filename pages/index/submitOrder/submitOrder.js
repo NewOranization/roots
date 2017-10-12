46d831fb408a16532a6cf24efe8e6fa779c9446f
金玉琪外 <1365401057@qq.com>
2017-10-12 18:42:36 +0800

    Revert "购物车功能以及提交订单页编写"
    This reverts commit 3c8c3a5648ca833ea41a0fecb137f93dc9a49572.

--- pages/index/submitOrder/submitOrder.js
+++ pages/index/submitOrder/submitOrder.js
@@ -1,39 +1,78 @@
 var app = getApp();
 Page({
   data: {
-    cartsData: [],
-    address: '',
+    time: '12:01',
+    submitData:{},
+    addressLen:0,
+    boxShow:false,
+    curSelTime:'',
+    curDate:'',
+    reMarkValue:''
   },
   onLoad: function (options) {
     var that = this;
-    var cartsData = wx.getStorageSync('cartsData');
-    var token = wx.getStorageSync('token');
-    var totalPrice = wx.setStorageSync('totalPrice', totalPrice);
-    var goodsInfo = [];
-
-    for(var i = 0; i < cartsData.length; i++){
-        if(cartsData[i].cartNum > 0){
-            goodsInfo.push({
-                gid: cartsData[i].id,
-                num: cartsData[i].cartNum
-            })
+    wx.getStorage({
+      key: 'toSubmitData',
+      success: function (res) {
+        var data = {
+          ac: 'handleorder',
+          op: 'orderCart',
+          push_token: res.data.push_token,
+          goodsInfo: res.data.goodsInfo,
+          oldPrice: res.data.oldPrice
         }
-    }
-    console.log(goodsInfo);
-    
-    var data = {
-        ac: 'handleorder',
-        op: 'orderCart',
-        push_token: token,
-        goodsInfo: goodsInfo,
-        oldPrice: totalPrice,
-    }
-    app.getPostData(function (post_data){
-        app.getApiData(function (res){
-            console.log(res)
-        }, 'GET', post_data)
-    }, data)
-   
+        app.getPostData(function (post_data) {//请求提交订单数据
+          app.getApiData(function (res) {
+            console.log(res.data.data);
+            that.setData({
+              submitData:res.data.data,
+              addressLen: res.data.data.address.length
+            })
+          }, 'GET', post_data)
+        }, data);
+      }
+    })
+    console.log(that.data.submitData)
   },
-
+  toSelTime:function(){
+    var that = this;
+    var curDate = new Date();
+    that.setData({
+      boxShow: true,
+      curDate: curDate.toLocaleDateString()
+    })
+  },
+  clickTime:function(e){
+    var that=this;
+    that.setData({
+      boxShow:false,
+      curSelTime: e.currentTarget.dataset.time
+    })
+  },
+  //页面跳转函数
+  toNext:function(e){
+    var that=this;
+    var click = e.currentTarget.dataset.click;
+    var next = e.currentTarget.dataset.next;
+    console.log(click);
+    console.log(next);
+    if (next=='address'){
+      wx.navigateTo({
+        url: '/pages/mine/address/index?click='+click
+      })
+    } else if (next == "quan") {
+      console.log("hahaha");
+      wx.navigateTo({
+        url: '/pages/index/coupon/coupon'
+      })
+    } else if (next == "hongbao") {
+      wx.navigateTo({
+        url: '/pages/index/hongbao/hongbao'
+      })
+    }else if (next =="ReMark"){
+      wx.navigateTo({
+        url: '/pages/index/reMark/reMark'
+      })
+    } 
+  }
 })
