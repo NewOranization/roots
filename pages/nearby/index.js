ba348297fdcc23dbf0fa9f7f39a2ed671be062b2
金玉琪外 <1365401057@qq.com>
2017-10-20 18:26:13 +0800

    提交

--- pages/nearby/index.js
+++ pages/nearby/index.js
@@ -1,158 +1,134 @@
 //index.js
 //获取应用实例
 var app = getApp();
+var page = 1;
 Page({
   data: {
-    top1: false,
-    top2: false,
-    top3: false,
+    isLoading: false,
+    headTitle: '附近商家',
     shopdata: {},
-    page: 1,
-    selNav: 1,
-    datalength: 0,
+    show1: false,
+    show2: false,
+    show3: false,
   },
-  loadMore: function (myData) {
-    var that = this;
-    var data = { ac: 'homepage', op: 'store' };
-    if (myData){
-      data = myData;
-    }
-    app.getPostData(function (post_data) {
-      app.getApiData(function (res) {
-        that.setData({
-          shopdata: res.data.data
-        })
-        console.log(that.data.shopdata)
-      }, 'GET', post_data)
-    },data);
-  },
-  onLoad: function (ops) {
-    var that = this;
-    that.loadMore();
-  },
-
-  againRequest: function (e) {
-    var that = this;
-    console.log(that.data.selNav);
-    var myData = {};
-    var cid = e.currentTarget.dataset.cid;
-    var soid = e.currentTarget.dataset.soid;
-    var did = e.currentTarget.dataset.did;
-    if (that.data.selNav == 1) {
-      console.log('商家分类')
-      myData = {
-        ac: 'homepage',
-        op: 'store',
-        cid: cid,//分类id
-      }
-    } else if (that.data.selNav == 2) {
-      console.log('只能排序')
-      myData = {
-        ac: 'homepage',
-        op: 'store',
-        soid: soid,//分类id
-      }
-    } else if (that.data.selNav == 3) {
-      console.log('优惠活动')
-      myData = {
-        ac: 'homepage',
-        op: 'store',
-        did: did,//分类id
-      }
-    }
-    that.loadMore(myData);
-    that.setData({
-      top1: false,
-      top2: false,
-      top3: false
-    })
-  },
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  // onReachBottom: function () {
-  //   var that = this;
-  //   if (that.data.datalength = 10) {
-  //     that.loadMore(that);
-  //   }
-  // },
-  /**
-  * 页面下拉触顶事件的处理函数
-  */
-  onPullDownRefresh: function () {
-    var that = this;
-    if (that.data.datalength = 10) {
-      that.loadMore(that);
-    }
-  },
-  RequestAll: function () {
-    var that = this;
-    that.setData({
-      shopInfo: [],
-      datalength: 0,
-      page: 1
-    })
-    app.getPostData(function (post_data) {
-      app.getApiData(function (res) {
-        console.log(res.data.data)
-        that.setData({
-          shopInfo: res.data.data,
-          page: that.data.page + 1
-        });
-        if (res.data.code == 0) {
-          wx.hideToast();
+  
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+        var that = this;
+        var data = {
+            ac: 'homepage',
+            op: 'store'
         }
-      }, 'GET', post_data)
-    }, { ac: 'homepage', op: 'store' });
-    that.setData({
-      top1: false,
-      top2: false,
-      top3: false
-    })
-  },
-  // **********************************************页面跳转函数
-  goSearchPage: function () {
-    wx.navigateTo({
-      url: '/pages/index/search/search'
-    })
-  },
-  toShopMenuList: function (e) {
-    var that = this;
-    var token = e.currentTarget.dataset.token;
-    wx.navigateTo({
-      url: '/pages/index/shopMenuList/shopMenuList?token=' + token
-    })
-  },
-  /**
-   ************************************************* tel_tab分类点击
-   */
-  navBarSwitch: function (e) {
-    var that = this;
-    var nav = e.currentTarget.dataset.nav;
-    that.setData({
-      selNav: nav
-    })
-    if (nav == 1) {
-      that.setData({
-        top1: !that.data.top1,
-        top2: false,
-        top3: false
-      })
-    }
-    if (nav == 2) {
-      that.setData({
-        top2: !that.data.top2,
-        top1: false,
-        top3: false
-      })
-    }
-    if (nav == 3) {
-      that.setData({
-        top3: !that.data.top3,
-        top2: false,
-        top1: false
-      })
-    }
+        that.getAll(data)
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 点击展开分类
+     */
+    openList: function (e){
+        var that = this;
+        var nav = e.currentTarget.dataset.nav;
+        console.log(nav)
+        if(nav == 1){
+            that.setData({
+                show1: !that.data.show1,
+                show2: false,
+                show3: false
+            })
+        }
+        if (nav == 2) {
+            that.setData({
+                show2: !that.data.show2,
+                show1: false,
+                show3: false
+            })
+        }
+        if (nav == 3) {
+            that.setData({
+                show3: !that.data.show3,
+                show2: false,
+                show1: false
+            })
+        }
+    },
+
+    /**
+     * 获得全部数据
+     */
+    getAll: function (data, page) {
+        var that = this;
+        app.getPostData(function (post_data){
+            app.getApiData(function (res){
+                if(res.data.code == 0){
+                    var shopdata = res.data.data;
+                    var length = shopdata.store.length;
+                    that.setData({
+                        shopdata: shopdata
+                    })
+                //     if (res.data.code == 0) {
+                //         if (page > 1) {
+                //             if (length >= 10) {
+                //                 that.data.shopdata.store.concat(shopdata.store)
+                //                 that.setData({
+                //                     shopdata: shopdata,
+            
+                //                 })
+                //             } else {
+                //                 that.data.shopdata.store.concat(shopdata.store)
+                //                 that.setData({
+                //                     shopdata: shopdata,
+
+                //                 })
+                //             }
+                //         } else {
+                //             if (length == 0) {
+                //                 that.setData({
+                //                     empty: true
+                //                 })
+                //             } else if (length >= 10) {
+                //                 that.setData({
+                //                     shopdata: shopdata,
+                //                     isLoading: false,
+                //                 })
+                //             } else {
+                //                 that.setData({
+                //                     shopdata: shopdata,
+                //                     isLoading: false,
+                //                 })
+                //             }
+                //         }
+                //     }
+                }
+            },'GET', post_data)
+        }, data)
+    },
+
 })
